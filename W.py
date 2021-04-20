f671cf992874568b0040dea0cdfed64c0c5b5d15
ishaagrawal67 <79989964+ishaagrawal67@users.noreply.github.com>
2021-04-20 12:36:23 +0530

    Add files via upload

---
+++ W.py
@@ -0,0 +1,8 @@
+for i in range(8):
+    for j in range(7):
+        if j==0 or j==6 or (i==4 and j==3) or (i==5 and (j==2 or j==4))or (i==6 and (j==1 or j==5)):
+            print("*",end="")
+        else:
+            print(end=" ")
+    print()
+
